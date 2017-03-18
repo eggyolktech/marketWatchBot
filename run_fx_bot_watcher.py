7260ba7cf02da95c98f7f036a992996e858eb24a
Egg Yolk Tech <eggyolktech@gmail.com>
2017-03-18 23:27:46 +0800

    Added Top 20 Support for TG Bot

--- run_fx_bot_watcher.py
+++ run_fx_bot_watcher.py
@@ -8,6 +8,7 @@ import sys
 import time
 import telepot
 from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
+from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton
 from bs4 import BeautifulSoup
 from decimal import Decimal
 import urllib.request
@@ -15,6 +16,11 @@ import urllib.parse
 import requests
 import random
 from get_daily_fx_calendar import get_fx_calendar
+from get_aastocks_hy_stats import get_aastocks_etf_stat, get_aastocks_hy_stat
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.properties')
 
 def on_chat_message(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
@@ -57,7 +63,13 @@ def on_chat_message(msg):
     
         keyboard = InlineKeyboardMarkup(inline_keyboard=keyboard_list)
         bot.sendMessage(chat_id, 'Please tap on the Commodity to quote', reply_markup=keyboard) 
-    
+  
+    elif (command == "/top10"):
+        
+        passage = 'Please tap on the Top 10 list to show \n\n' + ' /ttETF - High Dividends ETF ' + u'\U0001F4B0' + '\n\n' + ' /ttBanks - High Dividends Banks ' + u'\U0001F3E6' + '\n\n' + ' /ttREIT - High Dividends REIT ' + u'\U0001F3E2' + '\n\n' + ' /ttCong - Conglomerates ' + u'\U0001F310'
+        
+        bot.sendMessage(chat_id, passage, parse_mode='HTML')
+      
     elif (command == "/cal"):
     
         bot.sendMessage(chat_id, '<i>Retreiving Event Calendar...</i>', parse_mode='HTML')
@@ -69,22 +81,33 @@ def on_chat_message(msg):
         funny_list = ['http://cdn2.ettoday.net/images/1613/1613045.jpg', 'https://i0.wp.com/farm6.staticflickr.com/5462/9214555634_bb1859cd18.jpg', 'https://i1.wp.com/farm8.staticflickr.com/7412/9211776213_2229c5f440.jpg', 'https://i0.wp.com/farm6.staticflickr.com/5338/9214716454_19304819fe.jpg', 'http://i.imgur.com/1yTUg.jpg']
         
         f = urllib.request.urlopen(random.choice(funny_list))
-        bot.sendPhoto(chat_id, f)      
+        bot.sendPhoto(chat_id, f)     
+    elif (command.startswith("/tt")):
+    
+        bot.sendMessage(chat_id, '<i>Retreiving Top 10 List...</i>', parse_mode='HTML')
+        
+        industry = command[3:]
+        
+        if (industry == "ETF"):
+            passage = get_aastocks_etf_stat(config.get("aastocks","hy-url-etf"))
+            bot.sendMessage(chat_id, passage, parse_mode='HTML')        
+        else:
+            bot.sendMessage(chat_id, '<i>Sorry! Top 10 List does not exist for specified target [' + industry + '] </i>', parse_mode='HTML')
     else:
-        menu = '金鑊鏟 Bot v1.0.3 (不定期 Down 機) \n\n' + ' /fx - FX Quote ' + u'\U0001F4B9' + '\n\n' + ' /idq - Index Quote ' + u'\U0001F4C8' + '\n\n' + ' /cmd - Commodities Quote ' + u'\U0001F30E' + '\n\n' + ' /cal - Coming Events ' + u'\U0001F4C5' + '\n\n' + ' /funny - Time will tell... ' + u'\U0000231B'
+        menu = '金鑊鏟 Bot v1.0.3 \n\n' + ' /fx - FX Quote ' + u'\U0001F4B9' + '\n\n' + ' /idq - Index Quote ' + u'\U0001F4C8' + '\n\n' + ' /cmd - Commodities Quote ' + u'\U0001F30E' + '\n\n' + ' /cal - Coming Events ' + u'\U0001F4C5' + '\n\n' + ' /funny - Time will tell... ' + u'\U0000231B'
     
         bot.sendMessage(chat_id, menu)
         
 def on_callback_query(msg):
+
     query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
     print('Callback Query:', query_id, from_id, query_data)
+
+    # if query data is available
+    if query_data:
     
-    result = query_data + ": " + get_fx_live_rate(query_data)
-    
-    #for alert in PriceAlert.objects.filter(symbol=str(query_data),alert_status='1'):
-    #    result += "\n" + str(alert)
-    
-    bot.answerCallbackQuery(query_id, text=result)
+        result = query_data + ": " + get_fx_live_rate(query_data)
+        bot.answerCallbackQuery(query_id, text=result)
    
 def get_fx_live_rate(quote):
 
