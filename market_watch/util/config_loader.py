c3a1935cc56c3ba64795bd65ef630930d5fafe4e
eggyolktech@gmail.com <eggyolktech@gmail.com>
2017-06-19 00:33:09 +0800

    Make Config Loader a Singleton

--- market_watch/util/config_loader.py
+++ market_watch/util/config_loader.py
@@ -3,12 +3,38 @@
 import os
 import configparser
 
+class ConfigLoader:
+    __single = None
+    __conifg = None
+
+    def __init__(self):
+        if ConfigLoader.__single:
+            raise ConfigLoader.__single
+        ConfigLoader.__single = self
+ 
+        config = configparser.ConfigParser()
+        config_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'config.properties')
+        print(config_path)
+        config.read(config_path)
+        ConfigLoader.__config = config
+
+    def getSingleton():
+        if not ConfigLoader.__single:
+            ConfigLoader.__single = ConfigLoader()
+        return ConfigLoader.__single
+
+    def getConfig(self):
+        return ConfigLoader.__config
+
 def load():
 
-    config = configparser.ConfigParser()
-    config_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'config.properties')
-    print(config_path)
-    config.read(config_path)
+    singleton = ConfigLoader.getSingleton()
+    config = singleton.getConfig()
+    
+    #config = configparser.ConfigParser()
+    #config_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'config.properties')
+    #print(config_path)
+    #config.read(config_path)
     return config
 
 def main():
