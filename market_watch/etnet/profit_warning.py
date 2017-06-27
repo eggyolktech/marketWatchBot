3417a6d17fa5247c3973c437b904224c2d24d80c
eggyolktech@gmail.com <eggyolktech@gmail.com>
2017-06-28 02:32:20 +0800

    Added negative warning

--- market_watch/etnet/profit_warning.py
+++ market_watch/etnet/profit_warning.py
@@ -10,15 +10,21 @@ from datetime import datetime
 
 from market_watch.telegram import bot_sender
 
-def get_latest_reports(period):
+def get_latest_reports(period, reporttype=1):
 
     DEL = "\n\n"
     EL = "\n"
- 
-    url = "http://www.etnet.com.hk/www/tc/news/special_news_list.php?category=%E4%BC%81%E6%A5%AD%E7%9B%88%E5%96%9C"
     
+    if (reporttype == 1): 
+        url = "http://www.etnet.com.hk/www/tc/news/special_news_list.php?category=%E4%BC%81%E6%A5%AD%E7%9B%88%E5%96%9C"
+    elif (reporttype == 2):
+        url = "http://www.etnet.com.hk/www/tc/news/special_news_list.php?category=%E4%BC%81%E6%A5%AD%E7%9B%88%E8%AD%A6"
+    else:
+        print("Invalid Report Type")
+        return
+
     print("URL: [" + url + "]")  
-    
+ 
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
     r = requests.get(url, headers=headers)
@@ -50,7 +56,6 @@ def get_latest_reports(period):
             pdiff = now - pdate
             pdiff_mins = (pdiff.days * 24 * 60) + (pdiff.seconds/60)
             print("[Post @" + tag.text + " - " + str(pdiff_mins) + "mins old]")    
-        
             if (pdiff_mins > period):
                 print("Posts timestemps exceed monitoring period [" + str(period) + "mins], abort now.")
                 break
@@ -65,7 +70,13 @@ def get_latest_reports(period):
     
 def main():
 
-    for report in get_latest_reports(30):
+    # profit warning (+ve)
+    for report in get_latest_reports(30, 1):
+        print(report)
+        bot_sender.broadcast(report)
+
+    # profit warning (-ve)
+    for report in get_latest_reports(30, 2):
         print(report)
         bot_sender.broadcast(report)
    
