a6af7cfaf52f6cfadf827c60fb4e54004d2413a1
eggyolktech@gmail.com <eggyolktech@gmail.com>
2017-07-04 11:45:51 +0800

    Added WatchList Function

---
+++ market_watch/quick_watchlist.py
@@ -0,0 +1,137 @@
+#!/usr/bin/python
+
+import json
+
+QT = "/qt"
+EL = "\n"
+HK_FILE = "/app/hickoryStratsWatcher/web/json/hkTrackList.json"
+US_FILE = "/app/hickoryStratsWatcher/web/json/usTrackList.json"
+
+def init_track(region="HK"):
+    
+    # empty dictionary
+    data = {}
+    dump_dict(data, get_file(region))
+
+def get_file(region="HK"):
+
+    path = HK_FILE
+    if (region == "US"):
+        path = US_FILE
+
+    return path 
+
+def add_track(code):
+
+    data = None
+    region = None
+    
+    print("Code to Add: [" + code + "]")
+    
+    if (not code):
+        return
+    elif (is_number(code)):
+        data = load_dict("HK")
+        data[code] = code + ".HK"
+        region = "HK"
+    else:
+        data = load_dict("US")
+        if (not ".US" in code):
+            code = code + ".US"
+        data[code] = code
+        region = "US"
+    
+    if (data and region):
+        dump_dict(data, get_file(region))
+
+def list_track():
+
+    d1 = load_dict("HK")
+    d2 = load_dict("US")
+    m = ""
+    if (d1.items):
+        m = "HK Track List" + EL
+        for key, value in d1.items():
+            m = m + (key + " - " + value) + EL
+    
+    if (d2.items):
+        m = m + "US Track List" + EL
+        for key, value in d2.items():
+            m = m + (key + " - " + value) + EL
+
+    return m
+  
+def dump_dict(data, file):
+
+    with open(file, 'w') as fp:
+        json.dump(data, fp)
+   
+def load_dict(region="HK"):
+    
+    file = HK_FILE
+    if (region == "US"):
+        file = US_FILE
+
+    with open(file, 'r') as fp:
+        data = json.load(fp)
+        return data
+
+def remove_track(code):
+
+    data = None
+    region = None
+    
+    print("Code to Remove: [" + code + "]")
+    
+    if (not code):
+        return
+    elif (is_number(code)):
+        data = load_dict("HK")
+        if code in data:
+            del data[code]
+        else:
+            print("No key found!")
+            return
+        region = "HK"
+    else:
+        data = load_dict("US")
+        if code in data:
+            del data[code]
+        else:
+            print("No key found!")
+            return
+        region = "US"
+    
+    if (data and region):
+        dump_dict(data, get_file(region))
+ 
+def main():
+
+    init_track("HK")
+    init_track("US")
+    #print(load_file("HK"))
+    add_track("3800")
+    add_track("2628")
+    add_track("BABA")
+    add_track("GOOG")
+    add_track("FB")
+    print(list_track())
+    remove_track("2628")
+    remove_track("939")
+    remove_track("FB")
+    remove_track("AMZN")
+    print(list_track())
+
+def is_number(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+    
+if __name__ == "__main__":
+    main()                
+              
+
+
+
