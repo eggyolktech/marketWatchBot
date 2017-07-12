fe5f88aac0dd73b01cf1eb0e8d79b724b8815384
eggyolktech <eggyolktech@example.com>
2017-07-12 13:19:58 +0800

    Added Columns Alerts

---
+++ market_watch/google/columns_alerts.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+
+import feedparser
+from time import gmtime
+from datetime import datetime
+import time
+import requests
+
+from market_watch.telegram import bot_sender
+
+def get_columns_alerts(url):
+    
+    print("Url: [" + url + "]")
+    passage = ""
+
+    r = requests.get(url)
+    print(r.text)
+    
+
+    d = feedparser.parse(url)
+    print(d)
+    print(d.entries)
+	# print all posts
+    count = 1
+
+    print("\n" + str(datetime.fromtimestamp(time.mktime(gmtime()))))
+       	
+    for post in d.entries[:10]:
+       # get the difference in seconds        
+        elapse = time.mktime(gmtime()) - time.mktime(post.published_parsed)
+        print("Post #" + str(count) + " - " + post.published + " - " + str(elapse/60) + " mins ago")
+        #if(elapse/60 <= CHECK_PERIOD):
+        
+        passage = passage + "<b>" + post.title + "</b>" + "\n"
+        #passage = passage + post.description + "\n"
+        passage = passage + "@ " + post.published + "\n\n"
+        passage = passage + post.link + "\n\n"
+ 
+        count += 1
+
+    print("Total # of posts processed: %s" % (count-1))
+    
+    print("Passage: [" + passage + "]")
+    #passage = ""
+    return passage
+
+def main():
+    passage = get_columns_alerts('http://www.google.com.hk/alerts/feeds/17097742724167324955/851375453331312334')
+
+    # Send a message to a chat room (chat room ID retrieved from getUpdates)
+    if(passage):
+        bot_sender.broadcast(passage, True)
+
+if __name__ == "__main__":
+    main()        
+        
+
