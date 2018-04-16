2059d041f5fe224a096819ddb59ebeb2615ef3eb
eggyolktech <eggyolktech@gmail.com>
2018-04-16 23:12:35 +0800

    Added Twitter

--- market_watch/twitter/tweet.py
+++ market_watch/twitter/tweet.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 import twitter
+import time
+import sys
 import os
 from market_watch.util import config_loader
 from market_watch.telegram import bot_sender
@@ -10,6 +12,8 @@ import json
 config = config_loader.load()
 
 DEL = '\n\n'
+NEW_TWEET_COUNT = 30
+GET_TWEET_COUNT = 10
 
 API = twitter.Api(consumer_key=config.get("twitter","consumer_key"),
                     consumer_secret=config.get("twitter","consumer_secret"),
@@ -20,16 +24,6 @@ API = twitter.Api(consumer_key=config.get("twitter","consumer_key"),
 def push_tweet(name, tcount=1):
 
     sname = '@%s' % name
-    #user = API.GetUser(screen_name=sname)
-    #print("UserImg %s" % user.profile_image_url)
-    #bot_sender.broadcast_list(passage, "telegram-chat-test")
-      
-    try:
-        statuses = API.GetUserTimeline(screen_name=sname, count=10)
-    except:
-        print("User Timeline Error: [%s]" % name)
-        return
-    
     rkey = "Twitter:" + name.lower()
     json_arr = redis_pool.getV(rkey)
     tweet_list = []    
@@ -39,7 +33,16 @@ def push_tweet(name, tcount=1):
         json_arr = json_arr.decode()        
         tweet_list = json.loads(json_arr)
         print("Loaded Tweet List %s" % tweet_list)
-    
+        get_count = GET_TWEET_COUNT  
+    else:
+        get_count = NEW_TWEET_COUNT
+      
+    try:
+        statuses = API.GetUserTimeline(screen_name=sname, count=get_count)
+    except:
+        print("User Timeline Error: [%s]" % name)
+        return
+
     messages_list = []
     
     for s in statuses:
@@ -47,21 +50,23 @@ def push_tweet(name, tcount=1):
             print("%s created at %s is OLD! Skip sending...." % (s.id, s.created_at))
         else:
             print("%s created at %s is NEW! Prepare for sending...." % (s.id, s.created_at))
-            tweet_list.insert(0, str(s.id))
+            tweet_list.append(str(s.id))
             url = ('https://mobile.twitter.com/i/web/status/%s' % s.id)
             created = (s.created_at)
             text = (s.full_text)        
             message = "%s\n(<a href='%s'>%s</a>)" % (text, url, created)
             messages_list.append(message)
 
-    print("Full Tweet List %s" % tweet_list[:10])
-    new_json_arr = json.dumps(tweet_list[:10])
+    tweet_list.sort(reverse=True)
+    print("Full Tweet List %s" % tweet_list[:NEW_TWEET_COUNT])
+    new_json_arr = json.dumps(tweet_list[:NEW_TWEET_COUNT])
     redis_pool.setV(rkey, new_json_arr)    
 
     if messages_list:
         messages_list.insert(0, u'\U0001F30F' + "<b>@%s is Tweeting...</b>" % name)
         full_message = DEL.join(messages_list)
-        bot_sender.broadcast_list(full_message)
+        #bot_sender.broadcast_list(full_message)
+        bot_sender.broadcast_list(full_message, "telegram-notice")
     
 def get_tweet(name, tcount=1):
 
@@ -92,16 +97,19 @@ def get_tweet(name, tcount=1):
 def trump(tcount=1):
     return get_tweet('realDonaldTrump', tcount)
 
-def main():
-
-    WATCHER = ['realDonaldTrump','usstockcaptain','EmbassyofRussia']
-
-    #print("Hello")
-    #trump(5)
-    #get_tweet('usstockcaptain',10)
+def main(args):
     
-    for w in WATCHER:
-        push_tweet(w)
+    start_time = time.time()
+
+    if (len(args) > 1 and args[1] == "push_tweet"):
+        WATCHER = ['realDonaldTrump','usstockcaptain','RayDalio', 'webbhk', 'muddywatersre', 'stocktwits']
+        #WATCHER = ['realDonaldTrump']
+        for w in WATCHER:
+            push_tweet(w)
+    else:
+        trump(5)
     
+    print("Time elapsed: " + "%.3f" % (time.time() - start_time) + "s")    
+
 if __name__ == "__main__":
-    main() 
+    main(sys.argv)  
