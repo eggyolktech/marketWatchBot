575ee23f6578675e49dc9eeac0233edf117a9980
Egg Yolk Tech <eggyolktech@gmail.com>
2018-05-14 23:42:48 +0800

    Added finvinz heatmap

---
+++ market_watch/finviz/heatmap.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+
+from bs4 import BeautifulSoup
+from decimal import Decimal
+import urllib.request
+import urllib.parse
+import requests
+import re
+import os
+from datetime import date
+from datetime import datetime
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys  
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+def get_map():
+
+    DEL = "\n\n"
+    EL = "\n"
+
+    if (os.name == 'nt'):
+        options = Options()  
+        options.add_experimental_option("excludeSwitches",["ignore-certificate-errors"])
+        options.add_argument('--disable-gpu')
+        options.add_argument('--headless')
+        browser = webdriver.Chrome(executable_path="C:\Wares\chromedriver.exe", chrome_options=options)  
+    else:
+        browser = webdriver.PhantomJS() 
+
+    url = "https://finviz.com/map.ashx?t=sec"    
+    print("URL: [" + url + "]")
+
+    browser.implicitly_wait(3) # seconds
+    browser.get(url)
+   
+    try:
+        myDynamicElement = browser.find_element_by_id("share-map")
+        myDynamicElement.click()
+    except:
+        pass   
+        
+    print("Start dummy pass...") 
+    try:
+        myDynamicElement = browser.find_element_by_id("dummyid")
+    except:
+        pass
+   
+    try:
+        browser.implicitly_wait(3) # seconds
+        alert = browser.switch_to_alert()
+        error = alert.text
+        alert.accept()
+        print("alert accepted")
+        browser.close()
+    except:
+        print("no alert")
+    
+    html = browser.page_source
+    browser.close()
+    #print(html)
+    soup = BeautifulSoup(html, "html.parser")
+    divOverlay = soup.find("div", {"class": "overlay"})
+    imgurl = divOverlay.find("img")['src']
+    return imgurl
+    
+def main():
+
+    print(get_map())
+    
+def is_number(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+    
+if __name__ == "__main__":
+    main()                
+              
+
+
+
